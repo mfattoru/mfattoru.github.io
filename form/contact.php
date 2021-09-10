daae946d310a69915ca85190c8bd5f38db0ee2f0
Michele Fattoruso <michele.fattoruso@gmail.com>
2021-09-10 18:34:04 -0500

    updated few pages and images

--- form/contact.php
+++ form/contact.php
@@ -6,10 +6,10 @@ $mail = new PHPMailer();
 $emailTO = $emailBCC =  $emailCC = array(); $formEmail = '';
 
 ### Enter Your Sitename 
-$sitename = 'Your Site Name';
+$sitename = 'onofriofattorusoengineering.it';
 
 ### Enter your email addresses: @required
-$emailTO[] = array( 'email' => 'email@yoursite.com', 'name' => 'Your Name' ); 
+$emailTO[] = array( 'email' => 'fattoruso.engineeringsolutions@gmail.com', 'name' => 'Onofrio Fattoruso' ); 
 
 ### Enable bellow parameters & update your BCC email if require.
 //$emailBCC[] = array( 'email' => 'email@yoursite.com', 'name' => 'Your Name' );
